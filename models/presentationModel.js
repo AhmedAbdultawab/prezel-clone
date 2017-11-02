5f01a2c9e21ff36a9428ee8a9053c5fbd7561ee3
asawarik@andrew.cmu.edu <asawarik@andrew.cmu.edu>
2017-11-02 19:22:14 -0400

    added presentation model

---
+++ models/presentationModel.js
@@ -0,0 +1,29 @@
+mongodb://erwilson:sassycat@ds143245.mlab.com:43245/475database
+
+var MongoClient = require('mongodb').MongoClient;
+var mongoDB; 
+
+MongoClient.connect('mongodb://erwilson:475team@ds143245.mlab.com:43245/475database', (err, db) => {
+    if (err) return console.log(err);
+    mongoDB = db;
+});
+
+exports.create = function(collection, data, callback) {
+  mongoDB.collection(collection).insertOne(
+    data,                    
+    function(err, status) {   
+      if (err) doError(err);
+      var success = (status.result.n == 1 ? true : false);
+      callback(success,data);
+    });
+}
+
+
+
+
+
+
+
+
+
+
